24f75f757127a58bb64b25acfeb07a3b7436e9c4
frdella <112535662+frdella@users.noreply.github.com>
2022-10-10 13:42:59 +0700

    productcontr

--- example-app/app/Http/Controllers/ProductController.php
+++ example-app/app/Http/Controllers/ProductController.php
@@ -7,10 +7,8 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     public function show(){
-<<<<<<< HEAD
+
         return view('frontend.product.show');
-=======
         return view('fronted.product.show');
->>>>>>> 771846ac7a36a88590ffcee3eb60082ecadabf09
     }
 }
